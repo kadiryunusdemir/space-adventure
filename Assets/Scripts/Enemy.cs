3ed754560f95818b0134920aabdb1c7cca4a87b8
kadiryunusdemir <kadiryunusdemir@gmail.com>
2023-12-13 08:21:24 +0300

    add object collision detection to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,41 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float speed = 5;
 
+    private Rigidbody2D rb2D;
+    
     public void Init(Vector3 spawnPoint, Sprite sprite)
     {
         spriteRenderer.sprite = sprite;
         transform.position = spawnPoint;
         // transform.parent = spawnPoint;
         // transform.localScale = scale;
+    }
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + Vector2.down * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("BOOM");
+        }
+        else if (other.gameObject.CompareTag("Bullet"))
+        {
+            Debug.Log("Destroy asteroid");
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("this not be happen");
+        }
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -13,17 +13,38 @@ public class EnemySpawner : MonoBehaviour
     {
         Vector3 center = this.transform.position;
         float radius = 5f; 
+        int maxSpawnAttempts = 100; // Maximum number of attempts to find a non-overlapping position
+        float enemyRadius = 1f; // Assuming the enemy has a radius of 1 unit
+
         for (int i = 0; i < 10; i++)
         {
-            float angle = Random.Range(0f, 2f * Mathf.PI);
-            float distance = Random.Range(0f, radius);
+            Vector3 randomPoint;
+            int attempts = 0;
+            do
+            {
+                float angle = Random.Range(0f, 2f * Mathf.PI);
+                float distance = Random.Range(0f, radius);
 
-            float x = center.x + distance * Mathf.Cos(angle);
-            float y = center.y + distance * Mathf.Sin(angle);
-            var randomPoint = new Vector3(x, y, center.z);
-            
+                float x = center.x + distance * Mathf.Cos(angle);
+                float y = center.y + distance * Mathf.Sin(angle);
+                randomPoint = new Vector3(x, y, center.z);
+
+                // Check for overlaps near this position.
+                if(Physics2D.OverlapCircle(randomPoint, enemyRadius) == null) {
+                    // We found a non-overlapping spot! Slap it down & call it done.
+                    break;
+                }
+                attempts++;
+            } while(attempts < maxSpawnAttempts);
+
+            if(attempts >= maxSpawnAttempts) {
+                Debug.LogWarning("Failed to find a non-overlapping position after " + maxSpawnAttempts + " attempts.");
+                continue;
+            }
+
             var enemy = ObjectPoolManager.Instance.Get(Enums.ObjectPoolType.Enemy).GetComponent<Enemy>();
             enemy.Init(randomPoint, enemySprites[i % 2]);
         }
+
     }
 }
