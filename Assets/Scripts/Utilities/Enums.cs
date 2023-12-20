749db7a3da8b64fdcd79e502919999bd4790b3f7
kadiryunusdemir <kadiryunusdemir@gmail.com>
2023-12-20 21:42:05 +0300

    add game manager with game state action

--- Assets/Scripts/Utilities/Enums.cs
+++ Assets/Scripts/Utilities/Enums.cs
@@ -2,11 +2,30 @@ namespace Utilities
 {
     public static class Enums
     {
+        public enum GameState
+        {
+            Default = 0,
+            MainMenu,
+            Starting,
+            Playing,
+            Win,
+            Lose,
+            Paused,
+            GameEnded,
+        }
+        
         public enum ObjectPoolType
         {
             Default = 0,
-            Enemy = 1,
-            Bullet = 2,
+            Enemy,
+            Bullet,
+        };
+        
+        public enum LevelIndex
+        {
+            Default = 0,
+            Level1,
+            Level2,
         };
     }
 }
